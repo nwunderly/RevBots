48fc8fd9a39cf9f6d7ef0ad4d5419656efe3cdf1
minecrosters <48176165+minecrosters@users.noreply.github.com>
2020-03-01 14:39:47 -0500

    Update Tags.py

--- cogs/Tags.py
+++ cogs/Tags.py
@@ -1,11 +1,9 @@
 from discord.ext import commands
-# import yaml
-from utils.paginator import Pages
-from utils import mongo_setup
+from utils.paginator import Pages # import paginator file
 
 
-def tag_check(ctx, d: dict, tag):
-    t = d.get(tag, None)
+def tag_check(ctx, d: dict, tag): # i do recccccomend changing it to also be like permission based as well
+    t = d.get(tag, None) # how the format would be: {'tag_name': {'author': user id, 'value': 'tag value'}}
     if not t:
         return
     if t['author'] == ctx.author.id:
@@ -32,7 +30,7 @@ class Tag(commands.Cog):
             # print(tag)
             if not tag:
                 return await ctx.send("Please specify a Subcommand or tag")
-            tags = mongo_setup.tags.find_one({'_id': ctx.guild.id})
+            tags = mongo_setup.tags.find_one({'_id': ctx.guild.id}) # should be a dict formatted like: {'tag name': {'author': user id, 'value': 'tag value'}}
             if not tags:
                 return await ctx.send("This server has no tags yet")
             t = tags.get(tag, None)
@@ -45,15 +43,15 @@ class Tag(commands.Cog):
         tag_name = str(tag_name)
         if tag_name.lower() in ['remove', 'create']:
             return await ctx.send("Cannot make tag that is a tag command.")
-        if len(tag_name.strip()) > 100:
+        if len(tag_name.strip()) > 100: # tbh idk if this check is really a good one 
             return await ctx.send("Tag name too long (100 or less characters)")
         if len(str(tag_value)) >= 1800:
             return await ctx.send("Tag value too long (1800 or less characters")
-        g = mongo_setup.mod_and_logging_config.find_one({'_id': ctx.guild.id})
+        g = mongo_setup.mod_and_logging_config.find_one({'_id': ctx.guild.id}) # if theres tags for ctx.guild, then the dict should be formatted like: {'tag name': {'author': user id, 'value': 'tag value'}}
         if not g:
-            mongo_setup.tags.insert_one({'_id': ctx.guild.id})
+            mongo_setup.tags.insert_one({'_id': ctx.guild.id}) # if theres no tags for ctx.guild
             g = mongo_setup.tags.find_one({'_id': ctx.guild.id})
-        t = g.get(tag_name, None)
+        t = g.get(tag_name, None) # checking if the tag is already in the dict
         if not t:
             d = {'value': tag_value,
                  'author': ctx.author.id}
@@ -63,8 +61,8 @@ class Tag(commands.Cog):
 
     @tag.command()
     async def remove(self, ctx, tag_name):
-        g = mongo_setup.tags.find_one({'_id': ctx.guild.id})
-        if not g:
+        g = mongo_setup.tags.find_one({'_id': ctx.guild.id}) # if theres tags for ctx.guild, then the dict should be formatted like: {'tag name': {'author': user id, 'value': 'tag value'}}
+        if not g: # if ctx.guild has no tags
             return await ctx.send("This server has no tags")
         tag = g.get(tag_name, None)
         if not tag:
@@ -78,16 +76,16 @@ class Tag(commands.Cog):
 
     @commands.command()
     async def tags(self, ctx):
-        g = mongo_setup.tags.find_one({'_id': ctx.guild.id})
+        g = mongo_setup.tags.find_one({'_id': ctx.guild.id}) # if theres tags for ctx.guild, then the dict should be formatted like: {'tag name': {'author': user id, 'value': 'tag value'}}
         if not g:
             return await ctx.send("This server has no tags yet.")
         tag_info = []
         for key, val in g.items():
-            if not isinstance(val, dict):
+            if not isinstance(val, dict): # yea this is probably useless for juan so change this
                 continue
             author = val['author']
             tag_info.append(f'Name: {key}\nAuthor: {self.bot.get_user(author)}')
-        pages = Pages(ctx, entries=tag_info, specific_name=f'Tags for {ctx.guild.name}')
+        pages = Pages(ctx, entries=tag_info, specific_name=f'Tags for {ctx.guild.name}') # change this since the paginator for juan probably doesnt have specific_name, but it should
         await pages.paginate()
 
 
