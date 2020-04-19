712ae949a310c4c6b281a2a77ce3c51f1ec62421
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-04-19 14:14:23 -0400

    some quick fixes for juan and clippy

--- bots/clippy.py
+++ bots/clippy.py
@@ -1,6 +1,11 @@
 
+import io
 import asyncio
 import subprocess
+import traceback
+import textwrap
+import contextlib
+import datetime
 
 import discord
 from discord.ext import commands
@@ -10,7 +15,7 @@ from bots.revbot import RevBot
 from bots.evalbot import EvalCog
 
 
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 
 
 class Clippy(RevBot):
@@ -37,6 +42,7 @@ class Clippy(RevBot):
 class Systemd(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._last_result = None
 
     async def run_process(self, command):
         try:
@@ -47,6 +53,64 @@ class Systemd(commands.Cog):
             result = await self.bot.loop.run_in_executor(None, process.communicate)
 
         return [output.decode() for output in result]
+
+    @staticmethod
+    def cleanup_code(content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    @commands.is_owner()
+    @commands.command(pass_context=True, name='eval', aliases=['python', 'run'])
+    async def _eval(self, ctx, *, body: str):
+        """Runs arbitrary python code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_ret': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with contextlib.redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            # try:
+            #     await ctx.message.add_reaction('😎')
+            # except:
+            #     pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
     @commands.command()
     @commands.is_owner()
